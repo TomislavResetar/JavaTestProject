5ab68024c98536bc601cad1dcf49fcc66068d54c
sajka <sajka@sajka-laptop>
2018-07-10 17:23:03 +0200

    Something else

--- prvi_java_projekt/src/prvi_java_projekt/MyFirstJavaClass.java
+++ prvi_java_projekt/src/prvi_java_projekt/MyFirstJavaClass.java
@@ -5,7 +5,7 @@ public class MyFirstJavaClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int c;
+		int c; int d;
 		c=add2(2,3);
 		System.out.print(c);
 	}
