e95930ee1ca26b660d355f17e5c7265a34e95aba
sajka <sajka@sajka-laptop>
2018-07-10 21:54:27 +0200

    Added Global class

---
+++ prvi_java_projekt/src/prvi_java_projekt/Global.java
@@ -0,0 +1,9 @@
+package prvi_java_projekt;
+
+/**
+ * @author sajka
+ */
+
+public class Global {
+	 public static double PI = 3.14;
+}
--- prvi_java_projekt/src/prvi_java_projekt/MyFirstJavaClass.java
+++ prvi_java_projekt/src/prvi_java_projekt/MyFirstJavaClass.java
@@ -1,18 +1,51 @@
 package prvi_java_projekt;
 
+
 public class MyFirstJavaClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int c; int d;
+		int c; double d;
+		
+		Circle circ = new Circle();
+		circ.findArea(2.0);
+		
+		Circle circ2 = new Circle();
+		circ2.findArea(5.0);
+		
 		c=add2(2,3);
-		System.out.print(c);
+		d=add2(2.3,3.2);
+		System.out.println(c);
+		System.out.println(d);
+		
+		System.out.println(circ.area);
+		System.out.println(circ2.area);
 	}
 
 
 	public static int add2(int a, int b) {
+		System.out.println("Integer version called.");
 		return a+b;
+	}
+	
+	public static double add2(double a, double b) {
+		System.out.println("Double version called.");
+		return a+b;
+	}
+	
+	
+}
+
+class Circle
+{
+	double radius;
+	double area;
+	
+	public void findArea(double r)
+	{
+		radius=r;
+		area=r*r*Global.PI;
 	}
 	
 }
