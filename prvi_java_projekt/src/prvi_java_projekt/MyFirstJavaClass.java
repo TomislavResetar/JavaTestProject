330e01dae5594da94a593252824e9957e47dfc93
sajka <sajka@sajka-laptop>
2018-07-10 17:19:01 +0200

    First playing around with Java

---
+++ prvi_java_projekt/src/prvi_java_projekt/MyFirstJavaClass.java
@@ -0,0 +1,18 @@
+package prvi_java_projekt;
+
+public class MyFirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int c;
+		c=add2(2,3);
+		System.out.print(c);
+	}
+
+
+	public static int add2(int a, int b) {
+		return a+b;
+	}
+	
+}
